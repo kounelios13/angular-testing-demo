b8bb97ffda2b4181e2a8824c75b6d8fd5eb92222
Manos Kounelakis <pkounelios@gmail.com>
2019-01-10 00:33:04 +0200

    fixed tests

--- src/app/cars/cars.component.spec.ts
+++ src/app/cars/cars.component.spec.ts
@@ -2,24 +2,29 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CarsComponent } from './cars.component';
 import { ListComponent } from '../list/list.component';
-import {CarsService} from '../services/cars.service';
+import { CarsService } from '../services/cars.service';
 
 describe('CarsComponent', () => {
   let component: CarsComponent;
   let fixture: ComponentFixture<CarsComponent>;
-  let totalCars: number;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CarsComponent , ListComponent],
-      providers:[CarsService]
+      declarations: [CarsComponent, ListComponent],
+      providers: [CarsService]
     })
-    .compileComponents();
+      .compileComponents();
     fixture = TestBed.createComponent(CarsComponent);
     component = fixture.componentInstance;
-    totalCars = component.cars.length;
     fixture.detectChanges();
   }));
 
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
@@ -37,8 +42,11 @@ describe('CarsComponent', () => {
   });
 
 
-  it('should receive 2 cars from service',()=>{
-    const {cars} = component;
+  it('should receive 2 cars from service', async () => {
+    //We need to manually call component life cycle hooks
+    await component.ngOnInit();
+    fixture.detectChanges();
+    const cars = component.cars;
     expect(cars.length).toBe(2);
   });
 });
